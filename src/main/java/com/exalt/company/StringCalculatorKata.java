09e4dc7b2ec15251d7b8404f77cdd5bca6e03618
chakirardenne <ardenne.chakir@gmail.com>
2022-09-28 21:04:25 +0200

    feat: step five

--- src/main/java/com/exalt/company/StringCalculatorKata.java
+++ src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -1,52 +1,54 @@
 package com.exalt.company;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class StringCalculatorKata {
+    static final Predicate<String> isEmpty = String::isEmpty;
+    static final Predicate<Integer> isNegative = number -> number < 0;
 
-//    int add(String numbers) {
-//        throw new UnsupportedOperationException("Please, complete this method with your implementation");
-//    }
+    private String delimiter = ",";
 
-//    step one :
-//    int add(String numbers) {
-//        if(numbers.isEmpty())
-//            return 0;
-//        String[] strings = numbers.split(",", 2);
-//        return Integer.parseInt(strings[0]) + Integer.parseInt(strings[1]);
-//    }
-
-//    step two :
-//    int add(String numbers) {
-//        return Arrays.stream(numbers.split(","))
-//                .filter(((Predicate<String>) String::isEmpty).negate())
-//                .map(Integer::parseInt)
-//                .reduce(Integer::sum)
-//                .orElse(0);
-//    }
-
-//    step three :
-//    int add(String numbers) {
-//        return Arrays.stream(numbers.split("[,\n]"))
-//                .filter(((Predicate<String>) String::isEmpty).negate())
-//                .map(Integer::parseInt)
-//                .reduce(Integer::sum)
-//                .orElse(0);
-//    }
-
-//    step four :
     int add(String numbers) {
-        String delimiter = ",";
-        if(numbers.contains("//")) {
-            delimiter = String.valueOf(numbers.charAt(numbers.lastIndexOf("/") + 1));
-        }
-        if(!delimiter.equals(","))
-            numbers = numbers.substring(numbers.indexOf("\n"));
+        this.setNewDelimiter(numbers);
+        numbers = this.stripStringOfNewDelimiterLine(numbers);
+        this.hasNegative(numbers, delimiter);
         return Arrays.stream(numbers.split(String.format("[%s\n]", delimiter)))
-                .filter(((Predicate<String>) String::isEmpty).negate())
+                .filter(isEmpty.negate())
                 .map(Integer::parseInt)
                 .reduce(Integer::sum)
                 .orElse(0);
+    }
+
+    private String stripStringOfNewDelimiterLine(String numbers) {
+        if(!isDelimiterByDefault())
+            numbers = numbers.substring(numbers.indexOf("\n"));
+        return numbers;
+    }
+
+    private boolean isDelimiterByDefault() {
+        return delimiter.equals(",");
+    }
+
+    private void setNewDelimiter(String numbers) {
+        if(numbers.startsWith("//"))
+            delimiter = String.valueOf(numbers.charAt(numbers.lastIndexOf("/") + 1));
+    }
+
+    private void hasNegative(String numbers, String delimiter) {
+        List<Integer> negatives = this.getNegativesIntegers(numbers, delimiter);
+        if(!negatives.isEmpty())
+            throw new UnsupportedOperationException(String.format("Negatives numbers are not supported: %s", negatives));
+
+    }
+
+    private List<Integer> getNegativesIntegers(String numbers, String delimiter) {
+        return Arrays.stream(numbers.split(String.format("[%s\n]", delimiter)))
+                .filter(isEmpty.negate())
+                .map(Integer::parseInt)
+                .filter(isNegative)
+                .collect(Collectors.toList());
     }
 }
